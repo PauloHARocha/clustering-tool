ca06b580b87b59757d8311d29817df19e84eff5b
Paulo Rocha <ph.alcantara.rocha@gmail.com>
2018-08-31 14:52:07 -0300

    Adding more colors

--- js/main.js
+++ js/main.js
@@ -36,7 +36,16 @@ class Result{
 window.onload = function () {
     let form_submit = document.getElementById('form-submit');
     let result = {};
-    const colors = ['red', 'blue', 'green', 'yellow', 'orange', 'brown'];
+    const colors = ["green", "red", "yellow", "blue", "purple",
+        "orange", "brown", "pink", "gray", "cyan",
+        '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A',
+        '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+        '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC',
+        '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+        '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680',
+        '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+        '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3',
+        '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
     let form_ds = document.getElementById('dataset');
     let form_ag = document.getElementById('algorithm');
     let form_k = document.getElementById('k');
@@ -182,7 +191,7 @@ window.onload = function () {
                             markerType: "circle",
                             markerSize: 4,
                             name: `Cluster ${i}`,
-                            showInLegend: true,
+                            showInLegend: false,
                             dataPoints: clusters_chart
                         })
 
@@ -196,7 +205,7 @@ window.onload = function () {
                         markerType: "cross",
                         markerSize: 8,
                         name: "Centroid",
-                        showInLegend: true,
+                        showInLegend: false,
                         dataPoints: centroids_chart
                     });
                     centroids_chart = [];
